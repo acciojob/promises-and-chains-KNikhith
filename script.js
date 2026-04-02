b88bdcccde963c6f417b22865e7a821de0928d24
KNikhith <nikhithkotluri@gmail.com>
2026-04-02 11:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const form=document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+
+	let name=document.getElementById("name").value;
+	let age=Number(document.getElementById("age").value);
+
+	if(!name || !age){
+		alert("Please enter valid details.");
+		return;
+	}
+	let checkEligibility=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age> 18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000);
+	});
+	checkEligibility.then((message)=>alert(message))
+	.catch((message)=>alert(message));
+})
